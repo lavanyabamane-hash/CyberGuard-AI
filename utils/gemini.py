e180d7b898d97a5b955e96af321bf2c78068f8da
lavanyabamane-hash <32.lavanyabamane@gmail.com>
2026-08-05 00:40:16 +0530

    Create gemini.py

---
+++ utils/gemini.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+def get_gemini_response(prompt):
+    response = client.models.generate_content(
+        model="gemini-3.5-flash",
+        contents=prompt
+    )
+    return response.text
